849b701361cd77a94bfcce2cbe88526fa91084cf
godbhaal <godbhaal666@gmail.com>
2017-12-12 14:35:10 +0100

    Fix multiple case styles
    Convert all vars to snake_case to homogenize code.

--- source/session1.py
+++ source/session1.py
@@ -32,64 +32,64 @@ print('Loaded {} testing images filenames with classes {}'.
 
 # create the SIFT detector object
 
-SIFTdetector = cv2.SIFT(nfeatures=100)
+SIFT_detector = cv2.SIFT(nfeatures=100)
 
 # read the just 30 train images per class
 # extract SIFT keypoints and descriptors
 # store descriptors in a python list of numpy arrays
 
-Train_descriptors = []
-Train_label_per_descriptor = []
+train_descriptors = []
+train_label_per_descriptor = []
 
 for i in range(len(train_images_filenames)):
     filename = train_images_filenames[i]
     filename_path = path.join(DATA_PATH, filename)
-    if Train_label_per_descriptor.count(train_labels[i]) < 30:
+    if train_label_per_descriptor.count(train_labels[i]) < 30:
         print('Reading image ' + filename)
         ima = cv2.imread(filename_path)
         gray = cv2.cvtColor(ima, cv2.COLOR_BGR2GRAY)
-        kpt, des = SIFTdetector.detectAndCompute(gray, None)
-        Train_descriptors.append(des)
-        Train_label_per_descriptor.append(train_labels[i])
+        kpt, des = SIFT_detector.detectAndCompute(gray, None)
+        train_descriptors.append(des)
+        train_label_per_descriptor.append(train_labels[i])
         print(str(len(kpt)) + ' extracted keypoints and descriptors')
 
 # Transform everything to numpy arrays
 
-D = Train_descriptors[0]
-L = np.array([Train_label_per_descriptor[0]] * Train_descriptors[0].shape[0])
+D = train_descriptors[0]
+L = np.array([train_label_per_descriptor[0]] * train_descriptors[0].shape[0])
 
-for i in range(1, len(Train_descriptors)):
-    D = np.vstack((D, Train_descriptors[i]))
+for i in range(1, len(train_descriptors)):
+    D = np.vstack((D, train_descriptors[i]))
     L = np.hstack((L, np.array(
-        [Train_label_per_descriptor[i]] * Train_descriptors[i].shape[0])))
+        [train_label_per_descriptor[i]] * train_descriptors[i].shape[0])))
 
 # Train a k-nn classifier
 
 print('Training the knn classifier...')
-myknn = KNeighborsClassifier(n_neighbors=5, n_jobs=-1)
-myknn.fit(D, L)
+my_knn = KNeighborsClassifier(n_neighbors=5, n_jobs=-1)
+my_knn.fit(D, L)
 print('Done!')
 
 # get all the test data and predict their labels
 
-numtestimages = 0
-numcorrect = 0
+num_test_images = 0
+num_correct = 0
 for i in range(len(test_images_filenames)):
     filename = test_images_filenames[i]
     filename_path = path.join(DATA_PATH, filename)
     ima = cv2.imread(filename_path)
     gray = cv2.cvtColor(ima, cv2.COLOR_BGR2GRAY)
-    kpt, des = SIFTdetector.detectAndCompute(gray, None)
-    predictions = myknn.predict(des)
+    kpt, des = SIFT_detector.detectAndCompute(gray, None)
+    predictions = my_knn.predict(des)
     values, counts = np.unique(predictions, return_counts=True)
-    predictedclass = values[np.argmax(counts)]
+    predicted_class = values[np.argmax(counts)]
     print('image ' + filename + ' was from class ' +
-          test_labels[i] + ' and was predicted ' + predictedclass)
-    numtestimages += 1
-    if predictedclass == test_labels[i]:
-        numcorrect += 1
+          test_labels[i] + ' and was predicted ' + predicted_class)
+    num_test_images += 1
+    if predicted_class == test_labels[i]:
+        num_correct += 1
 
-print('Final accuracy: ' + str(numcorrect * 100.0 / numtestimages))
+print('Final accuracy: ' + str(num_correct * 100.0 / num_test_images))
 
 end = time.time()
 print('Done in ' + str(end - start) + ' secs.')
