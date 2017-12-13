bf7ed6f2b941ec592510c12a4e3c8dbae7fa8527
godbhaal <godbhaal666@gmail.com>
2017-12-13 20:20:50 +0100

    Fix generating descriptor labels

--- source/session1.py
+++ source/session1.py
@@ -9,6 +9,11 @@ from sklearn.neighbors import KNeighborsClassifier
 from descriptor_factory import SIFT
 from source import DATA_PATH
 
+generated_descriptors_path = os.path.join(DATA_PATH,
+                                          'generated_descriptors.dat')
+generated_labels_path = os.path.join(DATA_PATH,
+                                     'generated_labels.dat')
+
 
 def load_data(data_path):
     ## type: (str) -> tuple(list, list, list, list)
@@ -41,19 +46,15 @@ def load_data(data_path):
 
 
 def save_descriptors(descriptors, labels):
-    with open(os.path.join(DATA_PATH,
-                           'descriptors.dat'), 'w') as descriptors_file, \
-        open(os.path.join(DATA_PATH,
-                          'labels.dat'), 'w') as labels_file:
+    with open(generated_descriptors_path, 'w') as descriptors_file, \
+        open(generated_labels_path, 'w') as labels_file:
         cPickle.dump(descriptors, descriptors_file)
         cPickle.dump(labels, labels_file)
 
 
 def load_descriptors():
-    with open(os.path.join(DATA_PATH,
-                           'descriptors.dat'), 'r') as descriptors_file, \
-        open(os.path.join(DATA_PATH,
-                          'labels.dat'), 'r') as labels_file:
+    with open(generated_descriptors_path, 'r') as descriptors_file, \
+        open(generated_labels_path, 'r') as labels_file:
         descriptors = cPickle.load(descriptors_file)
         labels = cPickle.load(labels_file)
     return descriptors, labels
@@ -73,8 +74,12 @@ def assess(test_images, my_knn, descriptor_generator, test_labels):
         predictions = my_knn.predict(des)
         values, counts = np.unique(predictions, return_counts=True)
         predicted_class = values[np.argmax(counts)]
-        print('image ' + filename + ' was from class ' +
-              test_labels[i] + ' and was predicted ' + predicted_class)
+        print(
+            'image {} '
+            'was from class {} '
+            'and was predicted {}'.format(filename,
+                                          test_labels[i],
+                                          predicted_class))
         num_test_images += 1
         if predicted_class == test_labels[i]:
             num_correct += 1
@@ -90,7 +95,9 @@ def main():
     descriptor_generator = SIFT(number_of_features=100)
 
     # If descriptors are already computed load them
-    if os.path.isfile('descriptors.dat') and os.path.isfile('labels.dat'):
+    if os.path.isfile(generated_descriptors_path) and \
+        os.path.isfile(generated_labels_path):
+
         print('Loading descriptors...')
         D, L = load_descriptors()
     else:
@@ -99,17 +106,43 @@ def main():
         save_descriptors(D, L)
 
     # Train a k-nn classifier
-    print('Training the knn classifier...')
-    my_knn = KNeighborsClassifier(n_neighbors=5, n_jobs=-1)
-    my_knn.fit(D, L)
-    print('Done!')
+    classifier = KNN(n_neighbours=5)
+    classifier.train(D, L)
+    # train(descriptors=D, labels=L)
 
     num_correct, num_test_images = assess(test_images,
-                                          my_knn,
+                                          classifier,
                                           descriptor_generator,
                                           test_labels)
     print('Final accuracy: ' + str(num_correct * 100.0 / num_test_images))
     ## 30.48% in 302 secs.
+
+
+    # def train(descriptors, labels):
+    #     # Train a k-nn classifier
+    #     print('Training the knn classifier...')
+    #     my_knn = KNeighborsClassifier(n_neighbors=5, n_jobs=-1)
+    #     my_knn.fit(descriptors, labels)
+    #     print('Done!')
+    #     return my_knn
+
+
+class Classifier(object):
+    pass
+
+
+class KNN(Classifier):
+    # model should be accessible
+
+    def __init__(self, n_neighbours):
+        self.model = KNeighborsClassifier(n_neighbors=n_neighbours, n_jobs=-1)
+
+    def train(self, descriptors, labels):
+        self.model.fit(descriptors, labels)
+
+    def predict(self, descriptor):
+        predictions = self.model.predict(descriptor)
+        return predictions
 
 
 if __name__ == '__main__':
