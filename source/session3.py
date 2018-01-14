f2e3393c3e0a4de3be542ba5143d129b85acd847
godbhaal <godbhaal666@gmail.com>
2018-01-14 19:53:51 +0100

    Extract method to get a NN (loading it or generating it)

--- source/session3.py
+++ source/session3.py
@@ -1,13 +1,14 @@
-# Config for an specific GPU
-# import getpass
-# os.environ["CUDA_VISIBLE_DEVICES"]=getpass.getuser()[-1]
-
+import getpass
+import os
 import time
 
 from MLP import multi_layer_perceptron
-from source import DATA_PATH
+from source import DATA_PATCHES_PATH, DATA_PATH
 from utils import Color
 from utils import colorprint
+
+# Config to run on one GPU
+os.environ["CUDA_VISIBLE_DEVICES"] = getpass.getuser()[-1]
 
 # Compute SVM
 SVM = False
@@ -20,13 +21,12 @@ load_model = True
 
 MODEL_PATH = '../results/session3/my_first_mlp.h5'
 
-if __name__ == "__main__":
-    init = time.time()
 
+def get_nn(dataset_dir=DATA_PATH, load_model=False):
     # end to end MLP implementation
     neural_network = multi_layer_perceptron(img_size=32,
                                             batch_size=16,
-                                            dataset_dir=DATA_PATH,
+                                            dataset_dir=dataset_dir,
                                             model_fname=MODEL_PATH)
     neural_network.build_MLP_model()
 
@@ -36,21 +36,16 @@ if __name__ == "__main__":
         neural_network.plot_history()
     else:
         neural_network.load_MLP_model()
+    return neural_network
 
+
+if __name__ == "__main__":
+    init = time.time()
     if SVM:
-        # feature extraction with MLP + SVM classification
-        features, labels = neural_network.get_layer_output(
-            layer=neural_network.LAYERS.LAST, image_set='train')
-        if not cross_validate:
-            neural_network.train_classifier_SVM(features, labels)
-            features, labels = neural_network.get_layer_output(
-                layer=neural_network.LAYERS.LAST, image_set='test')
-            neural_network.evaluate_performance_SVM(features, labels,
-                                                    do_plotting=True)
-        else:
-            neural_network.cross_validate_SVM(features, labels)
+        get_nn(DATA_PATH, load_model)
 
     elif BoVW:
+        neural_network = get_nn(DATA_PATCHES_PATH, load_model)
         features, labels = neural_network.get_layer_output(
             layer=neural_network.LAYERS.LAST, image_set='train')
 
@@ -62,7 +57,9 @@ if __name__ == "__main__":
                                                      do_plotting=True)
         else:
             neural_network.cross_validate_BoVW(features, labels)
+
     else:
+        neural_network = get_nn(DATA_PATH)
         neural_network.plot_results()
 
     end = time.time()
