058e8b0983679fe37f8bec0520ad3f337a69aadc
godbhaal <godbhaal666@gmail.com>
2018-01-18 21:45:02 +0100

    Change new pooling layer name to pool

--- source/session4.py
+++ source/session4.py
@@ -97,7 +97,7 @@ def modify_model_before_block4(base_model):
         layer.trainable = False
 
     x = base_model.layers[-13].output
-    x = MaxPooling2D(pool_size=(4, 4), padding='valid')(x)
+    x = MaxPooling2D(pool_size=(4, 4), padding='valid', name='pool')(x)
     x = Flatten()(x)
     x = Dense(4096, activation='relu', name='fc1')(x)
     x = Dense(1024, activation='relu', name='fc2')(x)
